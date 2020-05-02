17f55c398f5c4be310ea78640e8c303357b51af4
jessamynsmith <jessamyn.smith@gmail.com>
2020-05-01 19:26:33 -0600

    Added initial points models

--- points/models.py
+++ points/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=40)
+    points_available = models.IntegerField()
+
+
+class Task(models.Model):
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    name = models.CharField(max_length=40)
+    description = models.TextField()
+    due_date = models.DateField
+    assigned_to = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
